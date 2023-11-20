cef4a1f8e53c62f1b487acf11ca69cf599f0cf11
Srini Pandiyan <srinipandiyan7@gmail.com>
2023-11-19 19:51:57 -0600

    added postgres client to db.js

--- db.js
+++ db.js
@@ -1,2 +1,10 @@
 /** Database setup for BizTime. */
+const { Client } = require("pg");
 
+const client = new Client({
+  connectionString: "postgresql:///biztime"
+});
+
+client.connect();
+
+module.exports = client;
