c858b9e54e9ac4586e42de7d632e1af2ee94e45f
Srini Pandiyan <srinipandiyan7@gmail.com>
2023-11-19 23:03:07 -0600

    put route

--- routes/companies.js
+++ routes/companies.js
@@ -68,13 +68,13 @@ router.post("/", async function (req, res, next){
     try {
         let {name, description} = req.body;
         let code = slugify(name, {lower: true});
-        const result = await db.query(
+        const query = await db.query(
             `INSERT INTO companies (code, name, description) 
              VALUES ($1, $2, $3) 
              RETURNING code, name, description`,
           [code, name, description]);
   
-      return res.status(201).json({"company": result.rows[0]});
+      return res.status(201).json({"company": query.rows[0]});
 
     } catch (err) {
         return next(err)
@@ -86,6 +86,28 @@ router.post("/", async function (req, res, next){
  * Needs to be given JSON like: {name, description} 
  * Returns update company object: {company: {code, name, description}}
 */
+
+router.put("/:code", async function (req, res, next) {
+    try{
+        let {name, description} = req.body;
+        let code = req.params.code;
+        const query = await db.query (
+            `UPDATE companies
+            SET NAME=$1, description $2
+            WHERE code = $3
+            RETURNING code, name, description`,
+            [name, description, code]
+        );
+
+        if(!query.rows.length){
+            throw new ExpressError(`Invalid company: ${code}`, 404)
+        } else {
+            return res.json({"company": query.rows[0]});
+        }
+    } catch(err){
+        return next(err)
+    }
+})
 
 /**
  * DELETE /companies/[code] : Deletes company. Should return 404 if company cannot be found.
